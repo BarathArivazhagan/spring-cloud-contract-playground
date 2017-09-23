8212ad850482efb1b2b5fa75b275a9dc796b17ed
Barath Arivazhagan <barathmacdec91@gmail.com>
2017-09-23 22:34:10 +0530

    code commit

---
+++ contract-producer/src/main/java/com/barath/cloud/app/ContractProducerApplication.java
@@ -0,0 +1,12 @@
+package com.barath.cloud.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractProducerApplication.class, args);
+	}
+}
